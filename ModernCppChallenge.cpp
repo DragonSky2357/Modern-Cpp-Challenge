7ac5a24e60fb755d6a1c475608a0563af4abed1f
unknown <qkrdydals327@gmail.com>
2021-06-23 16:09:56 +0900

    update 2021 06 23

--- ModernCppChallenge.cpp
+++ ModernCppChallenge.cpp
@@ -2876,8 +2876,8 @@ int main() {
 }
 */
 
-
-// 58 노드 사이의 최단 경로 계산
+/*
+// 58 노드 사이의 최단 경로 계산 (repeat)
 
 #include<iostream>
 #include<queue>
@@ -3099,5 +3099,270 @@ int main() {
 		}
 	}
 }
+*/
+
+/*
+// 59 족제비 프로그램
+
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<string_view>
+#include<random>
+#include<algorithm>
+#include<array>
+#include<iomanip>
+
+class weasel {
+	std::string target;
+	std::uniform_int_distribution<> chardist;
+	std::uniform_real_distribution<> ratedist;
+	std::mt19937 mt;
+	std::string const allowed_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+public:
+	weasel(std::string_view t) :
+		target(t), chardist(0, 26), ratedist(0, 100) {
+		std::random_device rd;
+		auto seed_data = std::array<int, std::mt19937::state_size>{};
+		std::generate(std::begin(seed_data), std::end(seed_data), std::ref(rd));
+		std::seed_seq seq(std::begin(seed_data), std::end(seed_data));
+		mt.seed(seq);
+	}
+
+	void run(const int copies) {
+		auto parent = make_random();
+		int step = 1;
+		std::cout << std::left << std::setw(5) << std::setfill(' ') << step << parent << std::endl;
+
+		do {
+			std::vector<std::string> children;
+			std::generate_n(
+				std::back_inserter(children),
+				copies,
+				[parent, this]() {return mutate(parent, 5); });
+
+			parent = *std::max_element(std::begin(children), std::end(children),
+				[this](std::string_view c1, std::string_view c2) {return fitness(c1) < fitness(c2); });
+
+			std::cout << std::setw(5) << std::setfill(' ') << step << parent << std::endl;
+			step++;
+		} while (parent != target);
+	}
+
+private:
+	weasel() = delete;
+
+	double fitness(std::string_view candidate) {
+		int score = 0;
+		for (size_t i = 0; i < candidate.size(); ++i) {
+			if (candidate[i] == target[i])
+				score++;
+		}
+
+		return score;
+	}
+
+	std::string mutate(std::string_view parent, const double rate) {
+		std::stringstream sstr;
+		for (const auto c : parent) {
+			auto nc = ratedist(mt) > rate ? c : allowed_chars[chardist(mt)];
+			sstr << nc;
+		}
+
+		return sstr.str();
+	}
+
+	std::string make_random() {
+		std::stringstream sstr;
+		for (size_t i = 0; i < target.size(); ++i)
+			sstr << allowed_chars[chardist(mt)];
+
+		return sstr.str();
+	}
+};
+
+int main(void) {
+	weasel w("METHINKS IT IS LIKE A WEASEL");
+	w.run(100);
+}
+*/
+
+// 60 생명 게임
+
+#include<iostream>
+#include<vector>
+#include<random>
+#include<array>
+#include<thread>
+#include<chrono>
+
+class universe {
+private:
+	universe() = delete;
+
+public:
+	enum class seed{
+		random,
+		ten_cell_row,
+		small_explorer,
+		explorer
+	};
+
+public:
+	universe(const size_t width, const size_t height) :
+		rows(height), columns(width), grid(width* height), dist(0, 4) {
+		std::random_device rd;
+		auto seed_data = std::array<int, std::mt19937::state_size>{};
+		std::generate(std::begin(seed_data), std::end(seed_data), std::ref(rd));
+		std::seed_seq seq(std::begin(seed_data), std::end(seed_data));
+		mt.seed(seq);
+	}
+
+	void run(const seed s, 
+		const int generations, 
+		const std::chrono::microseconds ms = std::chrono::milliseconds(100)) {
+		reset();
+		initialize(s);
+		display();
+
+		int i = 0;
+		do {
+			next_generation();
+			display();
+
+			using namespace std::chrono_literals;
+			std::this_thread::sleep_for(ms);
+		} while (i++ < generations || generations == 0);
+	}
+
+private:
+	void next_generation() {
+		std::vector<unsigned char> newgrid(grid.size());
+
+		for (size_t r = 0; r < rows; ++r) {
+			for (size_t c = 0; c < columns; ++c) {
+				auto count = count_neighbors(r, c);
+
+				if (cell(c, r) == alive) newgrid[r * columns + c] = (count == 2 || count == 3) ? alive : dead;
+				else newgrid[r * columns + c] = (count == 3) ? alive : dead;
+			}
+		}
+		grid.swap(newgrid);
+	}
+
+	void reset_display() {
+#ifdef _WIN32
+		system("cls");
+#endif
+	}
+
+	void display() {
+		reset_display();
+
+		for (size_t r = 0; r < rows; ++r) {
+			for (size_t c = 0; c < columns; ++c) {
+				std::cout << (cell(c, r) ? '*' : ' ');
+			}
+			std::cout << std::endl;
+		}
+	}
+
+	void initialize(seed const s) {
+		if (s == seed::small_explorer) {
+			auto mr = rows / 2;
+			auto mc = columns / 2;
+
+			cell(mc, mr) = alive;
+			cell(mc - 1, mr + 1) = alive;
+			cell(mc, mr + 1) = alive;
+			cell(mc + 1, mr + 1) = alive;
+			cell(mc - 1, mr + 2) = alive;
+			cell(mc + 1, mr + 2) = alive;
+			cell(mc, mr + 3) = alive;
+		} else if (s == seed::explorer) {
+			auto mr = rows / 2;
+			auto mc = columns / 2;
+
+			cell(mc - 2, mr - 2) = alive;
+			cell(mc, mr - 2) = alive;
+			cell(mc + 2, mr - 2) = alive;
+
+			cell(mc - 2, mr - 1) = alive;
+			cell(mc + 2, mr - 1) = alive;
+			cell(mc - 2, mr) = alive;
+			cell(mc + 2, mr) = alive;
+			cell(mc - 2, mr + 1) = alive;
+			cell(mc + 2, mr + 1) = alive;
+
+			cell(mc - 2, mr + 2) = alive;
+			cell(mc, mr - 2) = alive;
+			cell(mc + 2, mr + 2) = alive;
+		} else if (s == seed::ten_cell_row) {
+			for (size_t c = columns / 2 - 5; c < columns / 2 + 5; c++)
+				cell(c, rows / 2) = alive;
+		} else {
+			for (size_t r = 0; r < rows; ++r) {
+				for (size_t c = 0; c < columns; ++c) {
+					cell(c, r) = dist(mt) == 0 ? alive : dead;
+				}
+			}
+		}
+	}
+
+	void reset() {
+		for (size_t r = 0; r < rows; ++r) {
+			for (size_t c = 0; c < columns; ++c) {
+				cell(c, r) = dead;
+			}
+		}
+	}
 
 
+	int count_alive() { return 0; }
+
+	template<typename T1, typename... T>
+	auto count_alive(T1 s, T... ts) { return s + count_alive(ts...); }
+
+	int count_neighbors(size_t const row, size_t const col) {
+		if (row == 0 && col == 0)
+			return count_alive(cell(1, 0), cell(1, 1), cell(0, 1));
+		if (row == 0 && col == columns - 1)
+			return count_alive(cell(columns - 2, 0), cell(columns - 2, 1), cell(columns - 1, 1));
+		if (row == rows - 1 && col == 0)
+			return count_alive(cell(0, rows - 2), cell(1, rows - 2), cell(1, rows - 1));
+		if (row == rows - 1 && col == columns - 1)
+			return count_alive(cell(columns - 1, rows - 2), cell(columns - 2, rows - 2), cell(columns - 2, rows - 1));
+		if (row == 0 && col > 0 && col < columns - 1)
+			return count_alive(cell(col - 1, 0), cell(col - 1, 1), cell(col, 1), cell(col + 1, 1), cell(col + 1, 0));
+		if (row == rows - 1 && col > 0 && col < columns - 1)
+			return count_alive(cell(col - 1, row), cell(col - 1, row - 1), cell(col, row - 1), cell(col + 1, row - 1), cell(col + 1, row));
+		if (col == 0 && row > 0 && row < rows - 1)
+			return count_alive(cell(0, row - 1), cell(1, row - 1), cell(1, row), cell(1, row + 1), cell(0, row + 1));
+		if (col == columns - 1 && row > 0 && row < rows - 1)
+			return count_alive(cell(col, row - 1), cell(col - 1, row - 1), cell(col - 1, row), cell(col - 1, row + 1), cell(col, row + 1));
+
+		return count_alive(cell(col - 1, row - 1), cell(col, row - 1), cell(col + 1, row - 1), cell(col + 1, row), cell(col + 1, row + 1), cell(col, row + 1), cell(col - 1, row + 1), cell(col - 1, row));
+	}
+
+	unsigned char& cell(size_t const col, size_t const row) {
+		return grid[row * columns + col];
+	}
+private:
+	size_t rows;
+	size_t columns;
+
+	std::vector<unsigned char> grid;
+	const unsigned char alive = 1;
+	const unsigned char dead = 0;
+
+	std::uniform_int_distribution<> dist;
+	std::mt19937 mt;
+};
+
+int main() {
+	using namespace std::chrono_literals;
+
+	universe u(50, 20);
+	u.run(universe::seed::random, 100, 100ms);
+}
